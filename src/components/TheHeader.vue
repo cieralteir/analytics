b8687fe1f1146b998eaad22aa3dea87940ad6857
John Carlo Ricana <cier.alteir@gmail.com>
2020-08-23 15:44:41 +0800

    Install buefy
    Signed-off-by: John Carlo Ricana <cier.alteir@gmail.com>

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <b-navbar>
+    <template slot="brand">
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        KOLLAB
+      </b-navbar-item>
+    </template>
+  </b-navbar>
+</template>

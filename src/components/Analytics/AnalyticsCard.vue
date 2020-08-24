45cf8c5a8b9fd2b870e953b3f7889b7484ab3b93
John Carlo Ricana <cier.alteir@gmail.com>
2020-08-25 01:11:49 +0800

    Analytics cards
    Signed-off-by: John Carlo Ricana <cier.alteir@gmail.com>

---
+++ src/components/Analytics/AnalyticsCard.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card analytics-card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ title }}
+      </p>
+    </header>
+    <div class="card-content">
+      <div class="columns is-vcentered">
+        <div class="column">
+          <b-tag :type="type" size="is-medium">
+            {{ percentage }}% {{ percentageIncrease ? "+" : "-" }}
+          </b-tag>
+        </div>
+        <div class="column">
+          <div class="my-2" v-for="(dataSet, index) in dataSets" :key="index">
+            <p class="has-text-grey">{{ dataSet.label }}</p>
+            <p>{{ dataSet.value }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <slot name="footer">
+        <div class="card-footer-item">
+          {{ percentage }}%
+          {{ percentageIncrease ? "increase" : "decrease" }} in 30 days
+        </div>
+      </slot>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    dataSets: {
+      type: Array,
+      default: () => []
+    },
+    percentage: {
+      type: Number,
+    },
+    percentageIncrease: {
+      type: Boolean,
+    },
+    title: {
+      type: String,
+    },
+    type: {
+      type: String,
+      default: "is-primary",
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.analytics-card {
+  .card-footer-item {
+    border: none;
+    justify-content: start;
+  }
+}
+</style>
--- src/pages/analytics/index.vue
+++ src/pages/analytics/index.vue
@@ -1,7 +1,124 @@
 <template>
-  <div class="analytics-page">Analytics</div>
+  <div class="analytics-page">
+    <h1 class="title is-size-4">Analytics</h1>
+    <div class="columns is-multiline">
+      <div class="column is-4-desktop is-3-fullhd">
+        <analytics-card
+          title="Sales"
+          :percentage="analytics.sales.percentage"
+          :percentage-increase="analytics.sales.percentageIncrease"
+          :data-sets="[
+            {
+              label: 'Lifetime Sales',
+              value: analytics.sales.lifetimeSales,
+            },
+            {
+              label: 'Revenue Sales',
+              value: analytics.sales.revenueSales,
+            },
+          ]"
+        >
+          <template #footer>
+            <div class="card-footer-item">
+              Average Order Value
+            </div>
+            <div class="card-footer-item">
+              {{ analytics.sales.averageOrderValue }}
+            </div>
+          </template>
+        </analytics-card>
+      </div>
+      <div class="column is-4-desktop is-3-fullhd">
+        <analytics-card
+          title="Engagement"
+          :percentage="analytics.engagement.percentage"
+          :percentage-increase="analytics.engagement.percentageIncrease"
+          :data-sets="[
+            {
+              label: 'Lifetime SMS Engagements',
+              value: analytics.engagement.lifetimeSmsEngagements,
+            },
+            {
+              label: 'Daily Average',
+              value: analytics.engagement.dailyAverage,
+            },
+          ]"
+        />
+      </div>
+      <div class="column is-4-desktop is-3-fullhd">
+        <analytics-card
+          title="Acquisition"
+          :percentage="analytics.acquisition.percentage"
+          :percentage-increase="analytics.acquisition.percentageIncrease"
+          :data-sets="[
+            {
+              label: 'Registered Users',
+              value: analytics.acquisition.registeredUsers,
+            },
+            {
+              label: 'Daily Average',
+              value: analytics.acquisition.dailyAverage,
+            },
+          ]"
+        />
+      </div>
+      <div class="column is-4-desktop is-3-fullhd">
+        <analytics-card
+          title="Rewards Program"
+          :percentage="analytics.rewardsProgram.percentage"
+          :percentage-increase="analytics.rewardsProgram.percentageIncrease"
+          :data-sets="[
+            {
+              label: 'Rewards Redemeed',
+              value: analytics.rewardsProgram.rewardsRedeemed,
+            },
+            {
+              label: 'Issued Rewards',
+              value: analytics.rewardsProgram.issuedRewards,
+            },
+          ]"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import AnalyticsCard from "@/components/Analytics/AnalyticsCard";
+
+export default {
+  components: {
+    AnalyticsCard,
+  },
+  data: () => ({
+    // From API
+    analytics: {
+      sales: {
+        averageOrderValue: 1000,
+        lifetimeSales: 175000,
+        percentage: 20,
+        percentageIncrease: true,
+        revenueSales: 170000,
+      },
+      engagement: {
+        dailyAverage: 57,
+        lifetimeSmsEngagements: 285,
+        percentage: 20,
+        percentageIncrease: false,
+      },
+      acquisition: {
+        dailyAverage: 57,
+        registeredUsers: 285,
+        percentage: 20,
+        percentageIncrease: true,
+      },
+      rewardsProgram: {
+        issuedRewards: 175000,
+        percentage: 20,
+        percentageIncrease: true,
+        rewardsRedeemed: 175000,
+      },
+    },
+  }),
+};
 </script>
